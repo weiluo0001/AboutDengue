7e4270a547b6cb98a08e01451761daa47562a4b3
wei luo <weiluo0001@gmail.com>
2020-03-03 19:43:12 -0500

    Updates

---
+++ precache-manifest.178459ae912703e57200dbf04518fc92.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a95cc153c2ab488565803356235d453",
+    "url": "/AboutDengue/index.html"
+  },
+  {
+    "revision": "fdf9063083dd45599187",
+    "url": "/AboutDengue/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "f260802bc0ba9bd425d3",
+    "url": "/AboutDengue/static/css/main.fa708336.chunk.css"
+  },
+  {
+    "revision": "fdf9063083dd45599187",
+    "url": "/AboutDengue/static/js/2.5c751249.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/AboutDengue/static/js/2.5c751249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f260802bc0ba9bd425d3",
+    "url": "/AboutDengue/static/js/main.bfa759b9.chunk.js"
+  },
+  {
+    "revision": "d189f7120c350a9cb00c",
+    "url": "/AboutDengue/static/js/runtime-main.b0c0d7e0.js"
+  },
+  {
+    "revision": "3a4ce9d80ac5f2f8ad2b9e102be41249",
+    "url": "/AboutDengue/static/media/CityLevel.3a4ce9d8.bmp"
+  },
+  {
+    "revision": "0605e6f0111be1e91d70f61a3a3dad5f",
+    "url": "/AboutDengue/static/media/ForecastView.0605e6f0.bmp"
+  },
+  {
+    "revision": "2b5ec8b3a5a06fe15a015057c6f7ca7c",
+    "url": "/AboutDengue/static/media/WaveletView.2b5ec8b3.bmp"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AboutDengue/static/media/logo.5d5d9eef.svg"
+  }
+]);
